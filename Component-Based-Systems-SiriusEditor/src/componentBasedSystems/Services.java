96eaa8092ea0a3f9813d220bb7a8ca748688bffb
Simon Haug <sihait01@hs-esslingen.de>
2022-07-04 23:51:37 +0200

    move sirius editor to own project to add signature parameters

---
+++ Component-Based-Systems-SiriusEditor/src/componentBasedSystems/Services.java
@@ -0,0 +1,61 @@
+package componentBasedSystems;
+
+import componentbasedsystem.repository.Signature;
+import componentbasedsystem.repository.Parameter;
+import componentbasedsystem.repository.types.Type;
+import componentbasedsystem.repository.types.SimpleType;
+import componentbasedsystem.repository.types.Void;
+import componentbasedsystem.repository.types.ComplexType;
+import componentbasedsystem.repository.types.CollectionType;
+
+import org.eclipse.emf.common.util.EList;
+
+/**
+ * The services class used by VSM.
+ */
+public class Services {
+	public Services() {
+
+	}
+    
+	public String printParameters(Signature sig) {
+		String result = "";
+		EList<Parameter> parameters = sig.getParameters();
+		
+		if(parameters == null) // invalid list
+			return "";
+		
+		for (Parameter p : parameters) {
+			Type parameterType = p.getType();
+			String type = getDataTypeName(parameterType);
+			result += type + " " + p.getName() + ", ";
+		}
+		if (result.endsWith(", "))
+			result = result.substring(0, result.length() - 2);
+		return result;
+	}
+	
+	
+	public String getDataTypeName(Type dataType) {
+		String type = "";
+		if (dataType == null) {
+			type = "UNSPECIFIED";
+		} else if(dataType instanceof Void) {
+			type = "void";
+		} else if (dataType instanceof SimpleType) {
+			type = firstCharToLowerCase(((SimpleType) dataType).getType().name());
+		} else if ((dataType instanceof CollectionType)) {
+			String innerTypeString = getDataTypeName(((CollectionType) dataType).getInnerType());
+			String collectionTypeString = firstCharToLowerCase(((CollectionType) dataType).getType().name());
+			type = collectionTypeString + "<" + innerTypeString + ">";
+		} else if ((dataType instanceof ComplexType)) {
+			type = firstCharToLowerCase(((ComplexType) dataType).getName());
+		}
+
+		return type;
+	}
+	
+	private String firstCharToLowerCase(String original) {
+		return Character.toLowerCase(original.charAt(0)) + original.substring(1);
+	}
+}
